3a5bb42461b592f3a4aa40ca3435e91da54efbc6
Konstantine Dudkin <ttes7a@gmail.com>
2024-08-26 18:15:30 +0300

    prepare test build

--- webpack.config.ts
+++ webpack.config.ts
@@ -10,7 +10,7 @@ import {
 export default (env: BuildEnvironment) => {
   const paths: BuildPaths = {
     entry: path.resolve(__dirname, 'src', 'index.tsx'),
-    build: path.resolve(__dirname, 'dist'),
+    build: path.resolve(__dirname, 'build'),
     html: path.resolve(__dirname, 'public', 'index.html'),
   }
   const port: number = env.port || 3000
